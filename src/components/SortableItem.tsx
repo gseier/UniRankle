ba37cfd2e61655a97192e06252e62a094bd3addf
ArcticHere <seiergustav@gmail.com>
2025-10-19 16:11:51 +0200

    Drag Test

--- src/components/SortableItem.tsx
+++ src/components/SortableItem.tsx
@@ -10,6 +10,7 @@ interface SortableItemProps {
   onDragEnter: (e: React.DragEvent<HTMLDivElement>) => void;
   onDragEnd: (e: React.DragEvent<HTMLDivElement>) => void;
   isDragging: boolean;
+  onDrag: (e: React.DragEvent<HTMLDivElement>) => void;
   isSubmitted: boolean;
   correctRank?: number;
   rankingBy: RankVariable;
@@ -23,6 +24,7 @@ const SortableItem: React.FC<SortableItemProps> = ({
   onDragEnter,
   onDragEnd,
   isDragging,
+  onDrag,
   isSubmitted,
   correctRank,
   rankingBy,
@@ -58,6 +60,7 @@ const SortableItem: React.FC<SortableItemProps> = ({
     onDragStart={onDragStart}
     onDragEnter={onDragEnter}
     onDragEnd={onDragEnd}
+    onDrag={onDrag}
     onDragOver={(e) => e.preventDefault()}
     className={`flex flex-wrap sm:flex-nowrap items-stretch gap-3 
                 transition-all duration-300 transform bg-gray-50 
